ecf862dd30b67121671c7ac6c91129504d5ea95c
AlexxGorr <48887077+AlexxGorr@users.noreply.github.com>
2023-10-03 11:36:27 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,128 @@
+class Vertex:
+    def __init__(self):
+        self._links = []
+
+    @property
+    def links(self):
+        return self._links
+
+    @links.setter
+    def links(self, link):
+        self._links = link
+
+
+class Link:
+    def __init__(self, v1, v2):
+        self._v1 = v1
+        self._v2 = v2
+        self._dist = 1
+
+
+class LinkedGraph:
+    def __init__(self):
+        self._links = []
+        self._vertex = []
+
+    def add_vertex(self, v):
+        if v not in self._vertex:
+            self._vertex.append(v)
+
+    def add_link(self, link):
+        if link not in self._links:
+            self._links.append(link)
+
+    def find_path(self, start_v, stop_v):
+        pass
+
+
+# class Station(Vertex):
+#     def __init__(self, name):
+#         super().__init__()
+#         self._name = name
+#
+#     def __repr__(self):
+#         return f'{self._name}'
+#
+#
+# class LinkMetro(Link):
+#     def __init__(self, v1, v2, dist):
+#         super().__init__(v1, v2)
+#         self._dist = dist
+
+
+
+map_graph = LinkedGraph()
+
+v1 = Vertex()
+v2 = Vertex()
+v3 = Vertex()
+v4 = Vertex()
+v5 = Vertex()
+v6 = Vertex()
+v7 = Vertex()
+
+map_graph.add_link(Link(v1, v2))
+map_graph.add_link(Link(v2, v3))
+map_graph.add_link(Link(v1, v3))
+
+map_graph.add_link(Link(v4, v5))
+map_graph.add_link(Link(v6, v7))
+
+map_graph.add_link(Link(v2, v7))
+map_graph.add_link(Link(v3, v4))
+map_graph.add_link(Link(v5, v6))
+
+print(len(map_graph._links))   # 8 связей
+print(len(map_graph._vertex))  # 7 вершин
+path = map_graph.find_path(v1, v6)
+
+for i in map_graph.__dict__:
+    print(i)
+
+print(v1.links)
+
+
+
+################################################################
+
+
+
+# map_metro = LinkedGraph()
+# v1 = Station("Сретенский бульвар")
+# v2 = Station("Тургеневская")
+# v3 = Station("Чистые пруды")
+# v4 = Station("Лубянка")
+# v5 = Station("Кузнецкий мост")
+# v6 = Station("Китай-город 1")
+# v7 = Station("Китай-город 2")
+#
+# map_metro.add_link(LinkMetro(v1, v2, 1))
+# map_metro.add_link(LinkMetro(v2, v3, 1))
+# map_metro.add_link(LinkMetro(v1, v3, 1))
+#
+# map_metro.add_link(LinkMetro(v4, v5, 1))
+# map_metro.add_link(LinkMetro(v6, v7, 1))
+#
+# map_metro.add_link(LinkMetro(v2, v7, 5))
+# map_metro.add_link(LinkMetro(v3, v4, 3))
+# map_metro.add_link(LinkMetro(v5, v6, 3))
+#
+# print(len(map_metro._links))
+# print(len(map_metro._vertex))
+# path = map_metro.find_path(v1, v6)  # от сретенского бульвара до китай-город 1
+# print(path[0])    # [Сретенский бульвар, Тургеневская, Китай-город 2, Китай-город 1]
+# print(sum([x.dist for x in path[1]]))  # 7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
