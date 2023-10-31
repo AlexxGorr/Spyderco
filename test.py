bd78627ea102e6795005806229f141d615a9661e
AlexxGorr <48887077+AlexxGorr@users.noreply.github.com>
2023-10-31 16:51:07 +0500

    Update test.py

--- test.py
+++ test.py
@@ -10,20 +10,33 @@ class Ship:
         self._is_move = True
         self._cells = [1 for _ in range(length)]
 
-    def set_start_coords(x, y):
-        pass
+    def set_start_coords(self, x, y):
+        self._x, self._y = x, y
 
-    def get_start_coords():
-        pass
+    def get_start_coords(self):
+        return self._x, self._y
 
-    def move(go):
-        pass
+    def move(self, go):
+        go = 0
+        if self._is_move:
+            go += 1
 
-    def is_collide(ship):
-        pass
+    def is_collide(self, ship):
+        contour = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 0), (0, 1),
+            (1, -1), (1, 0), (1, 1),
+        ]
+        coord_collide = (0, 0)
+        for i in ship:
+            for dx, dy in contour:
+                coord_collide = i._x + dx, i._y + dy
+        if coord_collide == self.get_start_coords():
+            return True
 
-    def is_out_pole(size):
-        pass
+    def is_out_pole(self, size):
+        if self._x not in range(size) or self._y not in range(size):
+            return True
 
     def __getitem__(self, item):
         return self._cells[item]
@@ -49,16 +62,16 @@ class GamePole:
 
     def init(self):
         ships = [
-            Ship(4, rnd(1, 2)),
-            Ship(3, rnd(1, 2)),
-            Ship(3, rnd(1, 2)),
-            Ship(2, rnd(1, 2)),
-            Ship(2, rnd(1, 2)),
-            Ship(2, rnd(1, 2)),
-            Ship(1, rnd(1, 2)),
-            Ship(1, rnd(1, 2)),
-            Ship(1, rnd(1, 2)),
-            Ship(1, rnd(1, 2)),
+            Ship(4, tp=rnd(1, 2)),
+            Ship(3, tp=rnd(1, 2)),
+            Ship(3, tp=rnd(1, 2)),
+            Ship(2, tp=rnd(1, 2)),
+            Ship(2, tp=rnd(1, 2)),
+            Ship(2, tp=rnd(1, 2)),
+            Ship(1, tp=rnd(1, 2)),
+            Ship(1, tp=rnd(1, 2)),
+            Ship(1, tp=rnd(1, 2)),
+            Ship(1, tp=rnd(1, 2)),
         ]
         for sh in ships:
             self._ships.append(sh)
@@ -66,7 +79,7 @@ class GamePole:
     def get_ships(self):
         return [x for x in self._ships]
 
-    def move_ships():
+    def move_ships(self):
         pass
 
     def show(self):
@@ -86,6 +99,12 @@ pole.show()
 pole.move_ships()
 print()
 pole.show()
+
+
+
+
+
+
 
 
 
