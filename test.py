a7d4be172e9f73577ffc4f25d2dc0f96699febc7
AlexxGorr <48887077+AlexxGorr@users.noreply.github.com>
2023-11-09 16:06:29 +0500

    Update test.py

--- test.py
+++ test.py
@@ -41,11 +41,15 @@ class Ship:
             (0, -1), (0, 0), (0, 1),
             (1, -1), (1, 0), (1, 1),
         ]
+        res = []
         for i in ship:
             for dx, dy in contour:
-                cell_contour = i._x + dx, i._y + dy
-                if cell_contour[self._x][self._y] == 1:
-                    return True
+                cell_contour = i + dx, i + dy
+                res.append(cell_contour)
+                # if cell_contour[self._x][self._y] == 1:
+                #     return True
+                # return False
+        return res
 
     def is_out_pole(self, size):
         if self._x not in range(size) or self._y not in range(size):
@@ -67,12 +71,13 @@ class GamePole:
         self._size = size
         self._ships = []
         self._busy_cells = []
-        self._pole = [[0 for _ in range(size)] for _ in range(size)]
+        self._busy_contour = []
+        self._pole = [['-' for _ in range(size)] for _ in range(size)]
 
     def init(self):
         ships = [
-            # Ship(4, tp=rnd(1, 2)),
-            # Ship(3, tp=rnd(1, 2)),
+            Ship(4, tp=rnd(1, 2)),
+            Ship(3, tp=rnd(1, 2)),
             # Ship(3, tp=rnd(1, 2)),
             # Ship(2, tp=rnd(1, 2)),
             # Ship(2, tp=rnd(1, 2)),
@@ -86,36 +91,61 @@ class GamePole:
             self._ships.append(sh)
 
         for ship in self._ships:
-            ship_position = False
-            while not ship_position:
-                ship._x = rnd(0, self._size-1)
-                ship._y = rnd(0, self._size-1)
-                print(ship._x, ship._y)
-                ship_position = self.__check_position(ship._length, ship._tp, ship._x, ship._y)
+            ship._x = rnd(0, self._size-1)
+            ship._y = rnd(0, self._size-1)
+            if ship._tp == 1:
+                for i in range(ship._x, ship._x + ship._length):
+                    for j in range(ship._y, ship._y + 1):
+                        self._busy_cells.append((i, j))
+                        self._busy_contour.append(ship.is_collide((i, j)))
+            if ship._tp == 2:
+                for i in range(ship._x, ship._x + 1):
+                    for j in range(ship._y, ship._y + ship._length):
+                        self._busy_cells.append((i, j))
 
         for x, y in self._busy_cells:
             if 0 <= x <= 9 and 0 <= y <= 9:
-                self._pole[x][y] = 1
+                self._pole[x][y] = "■"
 
-    def __check_position(self, ln, orient, x, y):
-        if orient == 1 and (x + ln - 1) <= self._size-1 and (x, y) not in self._busy_cells:
-            for i in range(x-1, x+ln+1):
-                for j in range(y-1, y+2):
-                    if (i, j) not in self._busy_cells:
-                        self._busy_cells.append((i, j))
-                    else:
-                        continue
-            return True
-        elif orient == 2 and (y + ln - 1) <= self._size-1 and (x, y) not in self._busy_cells:
-            for i in range(x-1, x+2):
-                for j in range(y-1, y+ln+1):
-                    if (i, j) not in self._busy_cells:
-                        self._busy_cells.append((i, j))
-                    else:
-                        continue
-            return True
-        else:
-            return False
+        for cont in self._busy_contour:
+            for x, y in cont:
+                if 0 <= x <= 9 and 0 <= y <= 9:
+                    if self._pole[x][y] != "■":
+                        self._pole[x][y] = '*'
+
+    ################################################################################
+    #     for ship in self._ships:
+    #         ship_position = False
+    #         while not ship_position:
+    #             ship._x = rnd(0, self._size-1)
+    #             ship._y = rnd(0, self._size-1)
+    #             print(ship._x, ship._y)
+    #             ship_position = self.__check_position(ship._length, ship._tp, ship._x, ship._y)
+    #
+    #     for x, y in self._busy_cells:
+    #         if 0 <= x <= 9 and 0 <= y <= 9:
+    #             self._pole[x][y] = 1
+    #
+    # def __check_position(self, ln, orient, x, y):
+    #     if orient == 1 and (x + ln - 1) <= self._size-1 and (x, y) not in self._busy_cells:
+    #         for i in range(x-1, x+ln+1):
+    #             for j in range(y-1, y+2):
+    #                 if (i, j) not in self._busy_cells:
+    #                     self._busy_cells.append((i, j))
+    #                 else:
+    #                     continue
+    #         return True
+    #     elif orient == 2 and (y + ln - 1) <= self._size-1 and (x, y) not in self._busy_cells:
+    #         for i in range(x-1, x+2):
+    #             for j in range(y-1, y+ln+1):
+    #                 if (i, j) not in self._busy_cells:
+    #                     self._busy_cells.append((i, j))
+    #                 else:
+    #                     continue
+    #         return True
+    #     else:
+    #         return False
+    ################################################################################
 
     def get_ships(self):
         return [x for x in self._ships]
@@ -125,7 +155,7 @@ class GamePole:
 
     def show(self):
         for line in self._pole:
-            print(*list(map(lambda x: x, line)))
+            print(*line)
 
     def get_pole(self):
         return [[x for x in range(self._size)] for y in range(self._size)]
@@ -143,8 +173,8 @@ pole.show()
 
 print('-' * 30)
 print(pole._busy_cells)
-for i in pole._ships:
-    print(i.__dict__)
+print('-' * 30)
+print(pole._busy_contour)
 
 
 
